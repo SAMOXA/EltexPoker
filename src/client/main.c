e354e7b00cdcf078e97da5c4b8ce6e61ff2b58d4
SAMOXA <amdsamoxa@gmail.com>
2015-08-13 09:55:19 +0600

    Create base structure for autotools and make buildable

---
+++ src/client/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+	printf("Client\n");
+	return 0;
+}
---
+++ src/server/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	printf("Server\n");
+	return 0;
+}
