fd722cbb02ca76e15d98413246c6ff691d7e8a0b
SAMOXA <amdsamoxa@gmail.com>
2015-08-14 11:28:46 +0600

    Added structures for game server <--> client exchanging

--- src/global.h
+++ src/global.h
@@ -7,6 +7,16 @@
 #define MAX_TABLES_COUNT 5
 #define SESSION_TOKEN_LENGTH 16
 
+enum lears{
+	HEARTS,
+	DIAMONDS,
+	CLUBS,
+	SPADES
+};
+
+#define GET_CARD_LEAR(x) (x%4)
+#define GET_CARD_VALUE(x) ((unsigned char)x/4)
+
 struct loginRequest_t {
 	char name[MAX_NAME_LENGTH];
 	char pass[MAX_PASS_LENGTH];
@@ -33,4 +43,63 @@ struct selectResponce_t {
 	int port;
 }
 
+////////////////////From game server logic structures/////////
+enum gameStates {
+	START,
+	PRE_FLOP,
+	FIRST_ROUND,
+	FIRST_RISE,
+	FLOP,
+	ROUND,
+	ROUND_RISE,
+	TERN,
+	RIVER,
+	FINAL
+};
+
+enum playerStates {
+	ACTIVE,
+	PASSIVE,
+	PASS,
+	CONNECTING,
+	WAIT,
+	DISCONNECTED
+};
+
+
+struct player_t {
+	unsigned int id;
+	char name[MAX_NAME_LENGTH];
+	unsigned int state;
+	unsigned int bet;
+	unsigned int money;
+	unsigned char cards[2];
+};
+
+struct gameState_t {
+	unsigned int id;
+	unsigned char state;
+	unsigned char betPlayerIndex;
+	unsigned int bet;
+	unsigned char lastRisePlayerIndex;
+	unsigned int lastRiseCount;
+	unsigned int bank;
+	unsigned char cards[5];
+	player_t players[MAX_PLAYERS_PER_TABLE];
+};
+
+struct changeActivePlayer_t {
+	unsigned int id;
+	unsigned int lastBet;
+	unsigned int bank;
+};
+
+struct changeCardOnTable_t {
+	unsigned char cards[5];
+};
+
+struct changePlayerCards_t {
+	unsigned char cards[2];
+};
+//////////////////////////////////////////////////////
 #endif //GLOBAL_H
