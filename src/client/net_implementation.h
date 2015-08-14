207dea549731751b954a89aef7bdbe544b72dd29
markov <xxx@yyy.com>
2015-08-14 14:33:14 +0600

    special for Tima, very importatn changes

--- src/client/net_implementation.h
+++ src/client/net_implementation.h
@@ -9,15 +9,43 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
-int net_connect_server(char *addres, int port);
-void net_disconnect_server(int fd);
+/*
+* Create TCP scoket and connect to server 
+* return value: good "< 0", error "-1"
+*/
+int net_connect_server (char *addres, int port);
 
-int net_create_socket(int fd);
-struct sockaddr_in net_fill_socket_addr(struct sockaddr_in addr, 
-						char *addres,
-						int port);
-void net_connect(int fd, struct sockaddr_in server_addr);
-int net_send(int fd, void *buffer, int size);
-void *net_recive(int fd, void *buffer, int size);
+/*
+* Create TCP socket
+* return value: good "< 0", error "-1"
+*/
+int net_create_socket (int fd);
+
+/*
+* Connect to server 
+* return value: good "< 0", error "-1"
+*/
+int net_connect (int fd, struct sockaddr_in server_addr);
+
+/* 
+* Write message 
+* return value: good "< 0", error "-1"
+*/
+int net_send (int fd, void *buffer, int size);
+
+/* Disconnect from server */
+void net_disconnect_server (int fd);
+
+/*
+* Read message
+* return value: good !NULL, error NULL
+*/
+void *net_receive (int fd, void *buffer, int size);
+
+/*
+* Fill server structure 
+* return value: struct  sockaddr_in
+*/
+struct sockaddr_in net_fill_socket_addr (struct sockaddr_in addr, char *addres, int port);
 
 #endif
