f06dfd10adb54bdda7a7642dcd44a5de068f949c
potap <potapenkoalig@gmail.com>
2015-08-17 14:12:24 +0600

    Ncurses interface start vesion

---
+++ src/graf/graf.h
@@ -0,0 +1,116 @@
+#ifndef GRAF_H
+#define GRAF_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <termios.h>
+#include <ncurses.h>
+#include <locale.h>
+
+#include "graf_api.h"
+
+#define NCS_GRAF_CARD_SIZE_Y 4
+#define NCS_GRAF_CARD_SIZE_X 4
+
+#define NCS_GRAF_TABLE_SIZE_Y 24
+#define NCS_GRAF_TABLE_SIZE_X 80
+
+#define NCS_GRAF_CARD_NONE_COLOR COLOR_BLUE
+
+#define NCS_GRAF_INPUT_TITLE_SIZE 128
+#define NCS_GRAF_INPUT_BUFFER_SIZE 128
+
+#define NCS_GRAF_TABLE_BOTTOM 0
+#define NCS_GRAF_TABLE_LEFT 1
+#define NCS_GRAF_TABLE_TOP 2
+#define NCS_GRAF_TABLE_RIGHT 3
+
+#define NCS_GRAF_SPADES "♠"//пики
+#define NCS_GRAF_CLUBS "♣"//трефы
+#define NCS_GRAF_HEARTS "♥"//червы
+#define NCS_GRAF_DIAMONDS "♦"//бубны
+
+struct ncs_graf_card_t{
+    int color;
+    int selected;
+    char name[20];
+    unsigned int pos[2];
+    WINDOW *wnd;
+};
+
+struct ncs_graf_player_t{
+    int enabled;
+    char name[GRAF_MAX_NAME_SIZE];
+    char money_text[GRAF_MAX_MONEY_TEXT_SIZE];
+    char status_text[GRAF_MAX_STATUS_TEXT_SIZE];
+    unsigned int card_num;
+    struct ncs_graf_card_t *cards;
+    unsigned int pos[2];
+    unsigned int size[2];
+    unsigned int name_pos[2];
+    unsigned int status_pos[2];
+    unsigned int money_pos[2];
+    unsigned int cards_pos[2];
+    WINDOW *wnd;
+};
+
+struct ncs_graf_bank_t{
+    char money_text[GRAF_MAX_MONEY_TEXT_SIZE];
+    char timer_text[GRAF_MAX_TIMER_TEXT_SIZE];
+    unsigned int card_num;
+    struct ncs_graf_card_t *cards;
+    unsigned int pos[2];
+    unsigned int size[2];
+    unsigned int money_pos[2];
+    unsigned int cards_pos[2];
+    unsigned int timer_pos[2];
+    WINDOW *wnd;
+    
+//    WINDOW *timer_wnd;
+};
+
+struct ncs_graf_input{
+    int enabled;
+    int selected;
+    char title[NCS_GRAF_INPUT_TITLE_SIZE];
+    char buffer[NCS_GRAF_INPUT_BUFFER_SIZE];
+    unsigned int pos[2];
+    unsigned int size[2];
+    unsigned int title_pos[2];
+    unsigned int buffer_pos[2];
+    WINDOW *wnd;
+};
+
+struct ncs_graf_table_t{
+    struct ncs_graf_player_t** players;
+    struct ncs_graf_bank_t bank;
+    struct ncs_graf_input input;
+    unsigned int card_size[2];
+};
+
+void ncsGrafInitTable(	struct ncs_graf_table_t*,const struct graf_bank_t*,\
+			const int,const int);
+void ncsGrafInitPlayer(const struct ncs_graf_table_t*,\
+		    int,\
+		    int,const char*,\
+		    const char*,\
+		    const char*,\
+		    int,int);
+void ncsGrafSetCard(const struct ncs_graf_player_t*,\
+		int,int,const char*,const char*,\
+		int);
+void ncsGrafSetBankCard(const struct ncs_graf_bank_t*,\
+		int,int,const char*,const char*,\
+		int);
+void ncsStartGraf(struct ncs_graf_table_t*);
+void ncsInitColorPairs();
+int ncsSetColor(unsigned int,unsigned int);
+int ncsSetWndColor(WINDOW*,unsigned int,unsigned int);
+void ncsPrintInWnd(WINDOW*,const unsigned int*,const char*);
+void ncsShow(const struct ncs_graf_table_t*);
+void ncsShowBank(const struct ncs_graf_table_t*);
+void ncsShowInput(const struct ncs_graf_table_t*);
+void ncsEndGraf();
+
+#endif
---
+++ src/graf/graf_api.h
@@ -0,0 +1,62 @@
+#ifndef GRAF_API_H
+#define GRAF_API_H
+
+#define GRAF_MAX_PLAYERS 4
+
+#define GRAF_MAX_NAME_SIZE 128
+#define GRAF_MAX_MONEY_TEXT_SIZE 128
+#define GRAF_MAX_STATUS_TEXT_SIZE 128
+#define GRAF_MAX_TIMER_TEXT_SIZE 128
+
+#define GRAF_INDEX_NONE -1//отсутствие масти
+#define GRAF_INDEX_SPADES 0//пики
+#define GRAF_INDEX_CLUBS 1//трефы
+#define GRAF_INDEX_HEARTS 2//червы
+#define GRAF_INDEX_DIAMONDS 3//бубны
+
+struct graf_card_t{
+    int selected;
+    char val;
+    int index_suit;
+};
+
+struct graf_player_t{
+    int enabled;
+    char name[GRAF_MAX_NAME_SIZE];
+    char money_text[GRAF_MAX_MONEY_TEXT_SIZE];
+    char status_text[GRAF_MAX_STATUS_TEXT_SIZE];
+    unsigned int card_num;
+    struct graf_card_t *cards;
+//    unsigned int index_pos;//0 - пользователь
+};
+
+struct graf_bank_t{
+    char money_text[GRAF_MAX_MONEY_TEXT_SIZE];
+    char timer_text[GRAF_MAX_TIMER_TEXT_SIZE];
+    unsigned int card_num;
+    struct graf_card_t *cards;
+};
+
+struct graf_table_t{
+    struct graf_player_t players[GRAF_MAX_PLAYERS];
+    struct graf_bank_t bank;
+};
+
+void grafInit(struct graf_table_t*);
+void grafDrawAll();
+void grafDrawUserMsg(const char* msg);
+void grafDrawPlayer(int pos);
+void grafDrawBank(struct graf_bank_t*);
+void grafDrawTimer(const char* timer);
+void grafSetPlayer(struct graf_player_t*,int pos);
+void grafSetBank(struct graf_bank_t*);
+void grafExit();
+
+void grafShowInput(const char* title,const char* default_text);
+void grafHideInput();
+
+extern void (*graf_exit_event)(void);
+extern void (*graf_bet_event)(int sum);
+extern void (*graf_pass_event)(void);
+
+#endif
