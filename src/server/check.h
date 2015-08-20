5875376406d6c533e1ddc3a50dd5c32a311b797f
Litvinov Nikita <litviniv.nik.c@gmail.com>
2015-08-20 16:15:29 +0600

    merge master

---
+++ src/server/check.h
@@ -0,0 +1,8 @@
+#ifndef CHECK_H
+#define CHECK_H 
+#include <stdio.h>
+#include <regex.h>
+#include <sys/types.h>
+
+int checkIP(char *ip);
+#endif
--- src/server/main.c
+++ src/server/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include "server_network.h"
 #include "events.h"
+#include "check.h"
 
 int main(int argc, char **argv) {
+	if (argc < 3) {
+		printf("Input ip port\n");
+		return 0;
+	}
+	checkIP(argv[2]);
 	init_listen_server_network();
 	init();
 	listen_server_loop();
